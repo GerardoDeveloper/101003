315ec0359b38b8e027736bcc5879029c36da189d
Gerardo developer <gerardo.software.developer@gmail.com>
2020-12-04 10:43:00 -0300

    formateo e identación código

--- webviews/cambiar_banco/model/formulario.model.php
+++ webviews/cambiar_banco/model/formulario.model.php
@@ -1,98 +1,101 @@
 <?php
-	error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
 
-	$d = "hilos/101003/webviews/cambiar_banco";
-	include_once($_SERVER['DOCUMENT_ROOT'] . "/$d/inc/config.php");
-	include_once($_SERVER['DOCUMENT_ROOT'] . "/$d/inc/database.php");
-	
-	class FormularioModel {
-		private static $instancia;
-		private $conn;
+$d = "hilos/101003/webviews/cambiar_banco";
+include_once $_SERVER['DOCUMENT_ROOT'] . "/$d/inc/config.php";
+include_once $_SERVER['DOCUMENT_ROOT'] . "/$d/inc/database.php";
 
-		public function __construct() {
-			try {
-				$this->conn = Conexion::getInstance();
-			} catch (Exception $e) {
-				$error = "Error!: " . $e->getMessage();
+class FormularioModel
+{
+    private static $instancia;
+    private $conn;
 
-				die();
-			}
-		}
+    public function __construct()
+    {
+        try {
+            $this->conn = Conexion::getInstance();
+        } catch (Exception $e) {
+            $error = "Error!: " . $e->getMessage();
 
-		public function __destruct() {
-			$this->conn = null;
-		}
+            die();
+        }
+    }
 
-		public static function getInstance() {
-			if (!isset(self::$instancia)) {
-				$miclase = __CLASS__;
+    public function __destruct()
+    {
+        $this->conn = null;
+    }
 
-				self::$instancia = new $miclase;
-			}
+    public static function getInstance()
+    {
+        if (!isset(self::$instancia)) {
+            $miclase = __CLASS__;
 
-			return self::$instancia;
-		}
+            self::$instancia = new $miclase;
+        }
 
-		public function insertFormulario($identificador, $fecha_inicio, $origen) {
-			try {
-				$this->conn->setAttribute();
+        return self::$instancia;
+    }
 
-				$this->conn->beginTransaction();
-				
-				$sql = "INSERT INTO " . TB_FORMULARIO . " (identificador, fecha_inicio, origen) VALUES (:identificador, :fecha_inicio, :origen)";
-				
-				$query = $this->conn->prepare($sql);
+    public function insertFormulario($identificador, $fecha_inicio, $origen)
+    {
+        try {
+            $this->conn->setAttribute();
 
-				$query->bindParam(':identificador', $identificador, PDO::PARAM_STR);
-				$query->bindParam(':fecha_inicio', $fecha_inicio, PDO::PARAM_STR);
-				$query->bindParam(':origen', $origen, PDO::PARAM_STR);
+            $this->conn->beginTransaction();
 
-				if ($query->execute()) {
-					$status = 1;
-				} else {
-					$status = 0;
-				}
+            $sql = "INSERT INTO " . TB_FORMULARIO . " (identificador, fecha_inicio, origen) VALUES (:identificador, :fecha_inicio, :origen)";
 
-				$this->conn->commit();
-				
-				return [$status, ""];
-			} catch (PDOException $e) {
-				$this->conn->rollback();
+            $query = $this->conn->prepare($sql);
 
-				$error = $e->getMessage();
+            $query->bindParam(':identificador', $identificador, PDO::PARAM_STR);
+            $query->bindParam(':fecha_inicio', $fecha_inicio, PDO::PARAM_STR);
+            $query->bindParam(':origen', $origen, PDO::PARAM_STR);
 
-				return [0, $error];
-			}
-		}
-		
-		public function updateFormulario($identificador, $fecha_fin, $empresa, $apellidos, $nombres, $dni, $banco, $cbu, $fecha_ingreso, $provincia) {
-			try {
-				$this->conn->setAttribute();
+            if ($query->execute()) {
+                $status = 1;
+            } else {
+                $status = 0;
+            }
 
-				$this->conn->beginTransaction();
+            $this->conn->commit();
 
-				$sql = "UPDATE " . TB_FORMULARIO . " set fecha_fin = '$fecha_fin', empresa = '$empresa', apellidos = '$apellidos', nombres = '$nombres', dni = '$dni', banco = '$banco', cbu= '$cbu', fecha_ingreso = '$fecha_ingreso', provincia= '$provincia' WHERE identificador = '$identificador' ORDER BY id DESC LIMIT 1";
-				
-				$query = $this->conn->prepare($sql);
-				
-				if ($query->execute()) {
-					$status = 1;
-				} 
-				else {
-					$status = 0;
-				}
+            return [$status, ""];
+        } catch (PDOException $e) {
+            $this->conn->rollback();
 
-				$this->conn->commit();
+            $error = $e->getMessage();
 
-				return [$status, ""];
-			} 
-			catch (PDOException $e) {
-				$this->conn->rollBack();
+            return [0, $error];
+        }
+    }
 
-				$error = "Error!: " . $e->getMessage();
-								
-				return [0, $error];
-			}
-		}			
-	}
-?>
+    public function updateFormulario($identificador, $fecha_fin, $empresa, $apellidos, $nombres, $dni, $banco, $cbu, $fecha_ingreso, $provincia)
+    {
+        try {
+            $this->conn->setAttribute();
+
+            $this->conn->beginTransaction();
+
+            $sql = "UPDATE " . TB_FORMULARIO . " set fecha_fin = '$fecha_fin', empresa = '$empresa', apellidos = '$apellidos', nombres = '$nombres', dni = '$dni', banco = '$banco', cbu= '$cbu', fecha_ingreso = '$fecha_ingreso', provincia= '$provincia' WHERE identificador = '$identificador' ORDER BY id DESC LIMIT 1";
+
+            $query = $this->conn->prepare($sql);
+
+            if ($query->execute()) {
+                $status = 1;
+            } else {
+                $status = 0;
+            }
+
+            $this->conn->commit();
+
+            return [$status, ""];
+        } catch (PDOException $e) {
+            $this->conn->rollBack();
+
+            $error = "Error!: " . $e->getMessage();
+
+            return [0, $error];
+        }
+    }
+}
