6bea3d9151772a3f204a0c88961d9992fac05927
Gerardo developer <gerardo.software.developer@gmail.com>
2022-03-30 11:20:57 -0300

    - Se formateo código - Se cambiaron nombres de variables. - Se agrego la palabra que envíará el formulario al hilo

--- webviews/otras_consultas/inc/config.php
+++ webviews/otras_consultas/inc/config.php
@@ -1,16 +1,16 @@
 <?php
-const IN_PRODUCTION = true;
+const IN_PRODUCTION = false;
 
 if (IN_PRODUCTION) {
-	define("DB_HOST", "127.0.0.1");
-	define("DB_NAME", "_101003");
-	define("DB_USER", "root");
-	define("DB_PASSWORD", "homero");
+    define("DB_HOST", "127.0.0.1");
+    define("DB_NAME", "_101003");
+    define("DB_USER", "root");
+    define("DB_PASSWORD", "homero");
 } else {
-	define("DB_HOST", "127.0.0.1");
-	define("DB_NAME", "_101003");
-	define("DB_USER", "root");
-	define("DB_PASSWORD", "");
+    define("DB_HOST", "127.0.0.1");
+    define("DB_NAME", "_101003");
+    define("DB_USER", "root");
+    define("DB_PASSWORD", "");
 }
 
 define("LABS_CUENTA", "101003");
@@ -23,9 +23,9 @@ define("TB_TIPO_LICENCIA", "tipo_licencia");
 define("TB_DETALLE_LICENCIA", "detalle_licencia");
 
 $codes = array(
-	"200" => array(
-		"message" => "Envio consulta licencias",
-		"payload" => ""
-	)
+    "200" => array(
+        "message" => "Envio de cnsultas",
+        "payload" => "_otras consultas formulario",
+    ),
 );
 define("STATES_CODES", json_encode($codes));
