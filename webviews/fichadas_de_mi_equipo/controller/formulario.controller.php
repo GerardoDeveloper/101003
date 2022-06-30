b85502ec87fa62f91a2bd14a37b9da7e2a24c9a1
Gerardo developer <gerardo.software.developer@gmail.com>
2022-06-30 15:23:55 -0300

    - Se cambio path a 'fichadas_de_mi_equipo' - Se formateo código.

--- webviews/fichadas_de_mi_equipo/controller/formulario.controller.php
+++ webviews/fichadas_de_mi_equipo/controller/formulario.controller.php
@@ -1,139 +1,134 @@
 
 <?php
-	error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
 
-	$d = "hilos/101003/webviews/licencias";
-	include_once($_SERVER['DOCUMENT_ROOT'] . "/$d/model/formulario.model.php");
-	include_once($_SERVER['DOCUMENT_ROOT'] . "/$d/inc/functions.php");
-	
-	class FormularioController {	
-		private static $instancia;
-		private $modelInstancia;
-		private $formulario;
-		
-		public function __construct() {
-			try {
-				$this->modelInstancia = FormularioModel::getInstance();
-			} 
-			catch (Exception $e) {
-				$error = "Error!: " . $e->getMessage();
+$d = "hilos/101003/webviews/fichadas_de_mi_equipo";
+include_once $_SERVER['DOCUMENT_ROOT'] . "/$d/model/formulario.model.php";
+include_once $_SERVER['DOCUMENT_ROOT'] . "/$d/inc/functions.php";
 
-				die();
-			}
-		}
+class FormularioController
+{
+    private static $instancia;
+    private $modelInstancia;
+    private $formulario;
 
-		public static function getInstance() {
-			if (!isset(self::$instancia)) {
-				$miclase = __CLASS__;
+    public function __construct()
+    {
+        try {
+            $this->modelInstancia = FormularioModel::getInstance();
+        } catch (Exception $e) {
+            $error = "Error!: " . $e->getMessage();
 
-				self::$instancia = new $miclase;
-			}
+            die();
+        }
+    }
 
-			return self::$instancia;
-		}
+    public static function getInstance()
+    {
+        if (!isset(self::$instancia)) {
+            $miclase = __CLASS__;
 
-		public function getTiposLicencia()
-		{
-			$res = $this->modelInstancia->getTiposLicencia();
-			$options = "";
-			if(count($res) > 0)
-			{
-				foreach ($res as $key => $value) {
-					$clave = $value["id"];
-					$valor = $value["descripcion"];
-					$options .= "<option value='$clave'>$valor</option>\n";	
-				}
-			}
-			return $options;
-		}
+            self::$instancia = new $miclase;
+        }
 
-		public function getDetallesLicencia($licencia)
-		{
-			$res = $this->modelInstancia->getDetallesLicencia($licencia);
-			setLog($res[0], $res[1], false);
-			return $res;
-		}
+        return self::$instancia;
+    }
 
-		function insertFormulario($identificador, $fecha_inicio, $origen) {
-			$res = $this->modelInstancia->insertFormulario($identificador, $fecha_inicio, $origen);
-			setLog($res[0], $res[1], false);
-			return $res;
-		}
+    public function getTiposLicencia()
+    {
+        $res = $this->modelInstancia->getTiposLicencia();
+        $options = "";
+        if (count($res) > 0) {
+            foreach ($res as $key => $value) {
+                $clave = $value["id"];
+                $valor = $value["descripcion"];
+                $options .= "<option value='$clave'>$valor</option>\n";
+            }
+        }
+        return $options;
+    }
 
-		function updateFormulario($identificador, $fecha_fin, $licencia) {
-			$res = $this->modelInstancia->updateFormulario($identificador, $fecha_fin, $licencia);
-			setLog($res[0], $res[1], false);
-			return $res;
-		}		
-	}
-	
-	if (isset($_POST["function"]) AND !empty($_POST["function"])) {
-		$function = $_POST["function"];
-	} elseif (isset($_GET["function"]) AND !empty ($_GET["function"])) {
-		$function = $_GET["function"];
-	} else {
-		$function = NULL;
-	}
+    public function getDetallesLicencia($licencia)
+    {
+        $res = $this->modelInstancia->getDetallesLicencia($licencia);
+        setLog($res[0], $res[1], false);
+        return $res;
+    }
 
-	if ($function != NULL) {
-		switch(strtolower($function)) {
-			case "updateformulario":
-				echo updateFormulario();
-				break;
-			case "getdetalleslicencia":
-				echo getDetallesLicencia();
-				break;					
-		}
-	}
+    public function insertFormulario($identificador, $fecha_inicio, $origen)
+    {
+        $res = $this->modelInstancia->insertFormulario($identificador, $fecha_inicio, $origen);
+        setLog($res[0], $res[1], false);
+        return $res;
+    }
 
-	function getDetallesLicencia()
-	{
-		if (isset($_POST["licencia"]) && !empty($_POST["licencia"]))
-		{
-			$licencia = $_POST["licencia"];
-			$controller = FormularioController::getInstance();
-			$r = $controller->getDetallesLicencia($licencia);
-			return json_encode($r);
-		}
-		else
-		{
-			return false;
-		}
-	}
-	
-	function updateFormulario () {
-		$fecha = new DateTime();
-		$timestamp = $fecha->getTimestamp();
-		
-		if (isset($_POST["licencia"]) AND !empty($_POST["licencia"]))
-		{
-			$licencia = $_POST["licencia"];
-			$userid   = base64_decode($_POST["userid"]);
-		} 
-		else if (isset($_GET["licencia"]) AND !empty($_GET["licencia"]))
-		{
-			$licencia = $_GET["licencia"];
-			$userid   = base64_decode($_GET["userid"]);
-		} 
-		else {
-			return 0;
-		}
-		
-		$fecha_fin = date("Y/m/d H:i:s");
-		$controller = FormularioController::getInstance();
-		$r = $controller->updateFormulario($userid, $fecha_fin, $licencia);
-				
-		if ($r[0] == 1){
-			if(isset($_POST["conid"]) && !empty($_POST["conid"]))
-			{
-				$_SESSION["conid"] = $_POST["conid"];
-			}
-			setLog(200, $r[1], TRUE);
-		}
-		else{
-			setLog(0, $r[1]);
-		}
+    public function updateFormulario($identificador, $fecha_fin, $licencia)
+    {
+        $res = $this->modelInstancia->updateFormulario($identificador, $fecha_fin, $licencia);
+        setLog($res[0], $res[1], false);
+        return $res;
+    }
+}
 
-		return $r[0];
-	}	
+if (isset($_POST["function"]) and !empty($_POST["function"])) {
+    $function = $_POST["function"];
+} elseif (isset($_GET["function"]) and !empty($_GET["function"])) {
+    $function = $_GET["function"];
+} else {
+    $function = null;
+}
+
+if ($function != null) {
+    switch (strtolower($function)) {
+        case "updateformulario":
+            echo updateFormulario();
+            break;
+        case "getdetalleslicencia":
+            echo getDetallesLicencia();
+            break;
+    }
+}
+
+function getDetallesLicencia()
+{
+    if (isset($_POST["licencia"]) && !empty($_POST["licencia"])) {
+        $licencia = $_POST["licencia"];
+        $controller = FormularioController::getInstance();
+        $r = $controller->getDetallesLicencia($licencia);
+        return json_encode($r);
+    } else {
+        return false;
+    }
+}
+
+function updateFormulario()
+{
+    $fecha = new DateTime();
+    $timestamp = $fecha->getTimestamp();
+
+    if (isset($_POST["licencia"]) and !empty($_POST["licencia"])) {
+        $licencia = $_POST["licencia"];
+        $userid = base64_decode($_POST["userid"]);
+    } else if (isset($_GET["licencia"]) and !empty($_GET["licencia"])) {
+        $licencia = $_GET["licencia"];
+        $userid = base64_decode($_GET["userid"]);
+    } else {
+        return 0;
+    }
+
+    $fecha_fin = date("Y/m/d H:i:s");
+    $controller = FormularioController::getInstance();
+    $r = $controller->updateFormulario($userid, $fecha_fin, $licencia);
+
+    if ($r[0] == 1) {
+        if (isset($_POST["conid"]) && !empty($_POST["conid"])) {
+            $_SESSION["conid"] = $_POST["conid"];
+        }
+        setLog(200, $r[1], true);
+    } else {
+        setLog(0, $r[1]);
+    }
+
+    return $r[0];
+}
 ?>
--- webviews/fichadas_de_mi_equipo/inc/config.php
+++ webviews/fichadas_de_mi_equipo/inc/config.php
@@ -1,22 +1,21 @@
 <?php
-	define( "DB_HOST", "127.0.0.1" );
-	define( "DB_NAME", "_101003" );
-	define( "DB_USER", "root" );
-	define( "DB_PASSWORD", "homero" );
-	
-	define( "LABS_CUENTA", "101003");
-	
-	define( "FB_TOKEN", "");
-	
-	define( "TB_FORMULARIO", "formulario_licencias");
-	define( "TB_TIPO_LICENCIA", "tipo_licencia");
-	define( "TB_DETALLE_LICENCIA", "detalle_licencia");
-	
-	$codes = Array (
-		"200" => Array (
-			"message" => "Envio consulta licencias",
-			"payload" => ""
-		)
-	);
-	define( "STATES_CODES", json_encode($codes));
-?>
+define("DB_HOST", "127.0.0.1");
+define("DB_NAME", "_101003");
+define("DB_USER", "root");
+define("DB_PASSWORD", "homero");
+
+define("LABS_CUENTA", "101003");
+
+define("FB_TOKEN", "");
+
+define("TB_FORMULARIO", "formulario_fichadas_de_mi_equipo");
+define("TB_TIPO_LICENCIA", "tipo_licencia");
+define("TB_DETALLE_LICENCIA", "detalle_licencia");
+
+$codes = array(
+    "200" => array(
+        "message" => "Envio consulta fichadas de mi equipo",
+        "payload" => "",
+    ),
+);
+define("STATES_CODES", json_encode($codes));
